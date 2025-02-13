7753654a80ed53b262da8bc89aaabe433b8587e4
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2025-02-12 21:54:37 -0500

    init commit

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+// api/index.ts
+
+interface Env {
+  ASSETS: Fetcher;
+}
+
+export default {
+  fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/")) {
+      return Response.json({
+        name: "Cloudflare",
+      });
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+} satisfies ExportedHandler<Env>;
